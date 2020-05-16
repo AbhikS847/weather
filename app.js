3d68cfd7ea9b71422a233bf191df0f8a1ac0cd1d
Abhik <abhikshrestha@gmail.com>
2020-05-16 20:26:10 +1000

    Setup for Heroku

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require(`express`)
 const app = express()
+const port = process.env.PORT || 3000
 const path = require(`path`)
 const request = require(`request`)
 const bodyParser = require(`body-parser`)
@@ -38,6 +39,6 @@ app.post(`/`,(req,res)=>{
     })
 })
 
-app.listen(3000)
+app.listen(port)
 
 
